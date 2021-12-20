5208c5c1e6261ec50e97fe3c7309ecaa4fa5ccfa
valkyrie_pilot <valk@ferris.chat>
2021-12-20 13:44:55 -0700

    Initial github commit

--- src/choose_your_own_adventure.rs
+++ src/choose_your_own_adventure.rs
@@ -1,20 +1,23 @@
-pub fn wait(millis: u64) {
-    // Sleep for this many milliseconds
-    std::thread::sleep(std::time::Duration::from_millis(millis));
+#[macro_export]
+
+macro_rules! wait {
+    ($millis:literal) => {
+        // Sleep for this many milliseconds
+        std::thread::sleep(std::time::Duration::from_millis($millis));
+    }
 }
 
-pub fn choose(prompt: &str, options: Vec<String>) -> String {
+pub fn choose(options: Vec<String>) -> String {
     loop {
         let mut itr = 0;
         let mut choices: std::collections::HashMap<i8, String> = std::collections::HashMap::new();
         for option in &options {
             itr += 1;
-            wait(200);
+            wait!(200);
             println!("{}: {}", itr, option.to_string());
             choices.insert(itr, option.to_string());
         }
-        wait(200);
-        println!("{}", prompt);
+        wait!(200);
         let number_picked: i8 = text_io::read!("{}\n");
         match choices.get(&number_picked) {
             Some(review) => return review.to_string(),
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,41 @@
 
 mod choose_your_own_adventure;
 
+use choose_your_own_adventure::choose;
+
 fn main() {
     println!("You reach the island, and notice the many sheep wandering.");
-    let choice = choose_your_own_adventure::choose(
-        "Where do you go from here?",
-        vec!["The hill".to_string(), "The meadow".to_string()],
+    wait!(200);
+    println!("Do you go to the mountain to talk to the cyclops or try to steal the sheep?");
+    wait!(200);
+    let choice = choose(
+        vec!["Go to the mountain".to_string(), "Steal the sheep".to_string()],
     );
-    if choice == *"The hill" {
-        println!("went to hill");
-    } else if choice == *"The meadow" {
-        println!("went to meadow");
+    if choice == *"Go to the mountain" {
+        println!("You go to the mountain and go into the cyclops' cave, waiting for him to return.");
+        wait!(200);
+        println!("He returns, driving his sheep and goats, in to milk them");
+        wait!(200);
+        println!("What do you do next?");
+        let choice = choose(
+            vec!["Attack the cyclops".to_string(), "Ask to sit and eat with him".to_string(), "Try to run past the rock before he closes it".to_string()],
+        );
+        if choice == *"Attack the cyclops" {
+            println!("Not finished:tm:");
+        } else if choice == *"Ask to sit and eat with him" {
+            println!("The cyclops asks who you are and what your business is.");
+            wait!(200);
+            println!("How do you respond?");
+            let choice = choose( vec!["Travelers, and ask him to show hospitality".to_string(), "Warriors, and challenge him to fight".to_string()]);
+            if choice == *"Travelers, and ask him to show hospitality" {
+
+            } else if choice == *"Warriors, and challenge him to fight" {
+                println!("As the cyclops charges you, you stab at his ankles, but he crushes you underfoot and you are destroyed.");
+            }
+        } else if choice == *"Try to run past the rock before he closes it" {
+            println!("Not finished:tm:");
+        }
+    } else if choice == *"Steal the sheep" {
+        println!("Not implemented yet:tm:");
     }
 }
